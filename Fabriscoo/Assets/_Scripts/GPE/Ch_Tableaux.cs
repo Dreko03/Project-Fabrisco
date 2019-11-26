8d716ee996da5fe11c9daafc6b35dc6ca7810156
DrekoVR <Loic.pauleau@gmail.com>
2019-11-26 17:59:25 +0100

    Mise en place de GPEs simples

---
+++ Fabriscoo/Assets/_Scripts/GPE/Ch_Tableaux.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ch_Tableaux : MonoBehaviour
+{
+    Animator an;
+
+    private void Awake()
+    {
+        an = GetComponent<Animator>();
+    }
+
+    public void Change()
+    {
+        an.SetBool("rotateTab", true);
+    }
+
+    public void Unchange()
+    {
+        an.SetBool("rotateTab", false);
+    }
+}
--- Fabriscoo/Assets/_Scripts/Leak.cs
+++ Fabriscoo/Assets/_Scripts/Leak.cs
@@ -15,37 +15,35 @@ public class Leak : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (rb.isKinematic)
         {
             Debug.DrawRay(transform.position + new Vector3(0, 0.21f, 0), transform.up);
-            Debug.Log("IsKinematic");
             Ray ray = new Ray(transform.position + new Vector3(0, 0.21f, 0), transform.up);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, 9))
             {
-                Debug.Log("JE SUIS FACE AU SOL");
                 if (!ps_leak.isPlaying)
                     ps_leak.Play();
             }
             else
             {
-                Debug.Log("non");
                 if (ps_leak.isPlaying)
                     ps_leak.Stop();
             }
-            //if (rb.worldCenterOfMass.y > 3f)
-            //{
-            //    Debug.Log("IsRENVERSED");
-            //    ps_leak.Play();
-            //}
-            //else
-            //{
-            //    ps_leak.Stop();
-            //}
         }
         else
         {
             ps_leak.Stop();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(rb.velocity.y > 1f || rb.velocity.y < -1f)
+        {
+            Debug.Log("Velocity at Col : " + rb.velocity.y);
+            Destroy(gameObject);
         }
     }
 }
