c40e57c148e02079186fd8c4149936fa455c08a2
Loïc Pauleau <loic.pauleau@gmail.com>
2019-11-29 17:10:24 +0100

    Add Piste Disco
    pisteDisco.cs
    pistedisco (cubes + lights)
    emissive Materials

---
+++ Fabriscoo/Assets/_Scripts/Piste disco/PisteDisco.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PisteDisco : MonoBehaviour
+{
+
+    public Sequence[] sequences = new Sequence[4];
+    public int currentSequence = 0;   
+
+    void Start()
+    {
+        foreach (Sequence sequence in sequences)
+        {
+            sequence.TurnOff();
+        }
+        sequences[currentSequence].TurnOn();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    [System.Serializable]
+    public class Sequence
+    {
+        public Light[] cells;
+
+        public void TurnOff()
+        {
+            for (int i = 0; i < cells.Length; i++)
+            {
+                cells[i].enabled = false;
+            }
+        }
+
+        public void TurnOn()
+        {
+            foreach (Light light in cells)
+            {
+                light.enabled = true;
+            }
+        }
+    }
+}
+
+
