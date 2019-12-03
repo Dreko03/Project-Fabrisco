af60390e7800018a023ea4936d5cd0dea7e18d69
DrekoVR <Loic.pauleau@gmail.com>
2019-12-03 18:05:06 +0100

    Scratching

--- Fabriscoo/Assets/_TEST/SC_Disc.cs
+++ Fabriscoo/Assets/_TEST/SC_Disc.cs
@@ -9,7 +9,12 @@ public class SC_Disc : MonoBehaviour
     public Animator[] ans;
     public AnimationState anims;
     public CircularDrive circularDrive;
+    public Interactable interactComponent;
+    public LinearMapping linearMapping;
     public float outAngle;
+    public float rotationValue;
+    public float linearMapValue;
+    public float globalMultiplier;
 
     // Start is called before the first frame update
     void Awake()
@@ -18,31 +23,63 @@ public class SC_Disc : MonoBehaviour
         ans = FindObjectsOfType<Animator>();
         outAngle = circularDrive.outAngle;
         circularDrive = GetComponent<CircularDrive>();
+        interactComponent = GetComponent<Interactable>();
+        linearMapping = GetComponent<LinearMapping>();
+    }
+
+    public void Update()
+    {
+        transform.Rotate(0, rotationValue * Time.deltaTime, 0, Space.World);
+        if (linearMapping.value > linearMapValue)
+        {
+            Accelerate(linearMapping.value - linearMapValue);
+        }
+        else if (linearMapping.value < linearMapValue)
+        {
+            Decelerate(linearMapValue - linearMapping.value);
+        }
+        else
+        {
+            Reset();
+        }
+        linearMapValue = linearMapping.value;
     }
 
     // Update is called once per frame
-    public void Accelerate()
+    public void Accelerate(float multiplier)
+    {
+        foreach (AudioSource As in AudioSources)
+        {
+            As.pitch += multiplier * globalMultiplier;
+        }
+
+        foreach (Animator an in ans)
+        {
+            an.SetFloat("Multiplier", 1 + (multiplier * globalMultiplier));
+        }
+    }
+
+    public void Decelerate(float multiplier)
+    {
+        foreach (AudioSource As in AudioSources)
+        {
+            As.pitch -= multiplier * globalMultiplier;
+        }
+        foreach (Animator an in ans)
+        {
+            an.SetFloat("Multiplier", -1 - (multiplier * globalMultiplier));
+        }
+    }
+
+    public void Reset()
     {
         foreach (AudioSource As in AudioSources)
         {
             As.pitch = 1f;
         }
-
         foreach (Animator an in ans)
         {
             an.SetFloat("Multiplier", 1f);
-        }
-    }
-
-    public void Decelerate()
-    {
-        foreach (AudioSource As in AudioSources)
-        {
-            As.pitch = -1f;
-        }
-        foreach (Animator an in ans)
-        {
-            an.SetFloat("Multiplier", -1f);
         }
     }
 }
