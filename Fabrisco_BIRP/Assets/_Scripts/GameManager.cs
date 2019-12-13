487e6385003e33fff5b4f55b75977de084cb3ce1
DrekoVR <Loic.pauleau@gmail.com>
2019-12-13 15:14:03 +0100

    TextureTests

---
+++ Fabrisco_BIRP/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+
+public class GameManager : MonoBehaviour
+{
+    public bool discoActive;
+    public AudioSource[] AudioSources;
+    public Transform CamPos;
+    public Collider[] viewedObjects;
+    public float capsuleLength, capsuleRadius;
+    public ChangeTheWorld[] GPEs;
+    public float fps, timeScale;
+    public Text fpstxt, timetxt;
+    public PlayableDirector tml_transition;
+    public float timers;
+    public GameObject UI_Micro;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GPEs = FindObjectsOfType<ChangeTheWorld>();
+        //Time.captureFramerate = 90;
+        Application.targetFrameRate = 90;
+
+    }
+
+    public void Activate()
+    {
+        tml_transition.Play();
+    }
+
+    public void Desactivate()
+    {
+        discoActive = false;
+        foreach (AudioSource As in AudioSources)
+        {
+            As.Stop();
+            As.pitch = 1f;
+        }
+
+        foreach (ChangeTheWorld reverseEvent in GPEs)
+        {
+            reverseEvent.isChanged = false;
+            reverseEvent.RevertChanges.Invoke();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();
+        FPScount();
+        timeScale = Time.timeScale;
+
+        if (discoActive)
+        {
+            //viewedObjects = Physics.OverlapCapsule(CamPos.position, CamPos.position + new Vector3(0, 0, capsuleLength), capsuleRadius);
+            viewedObjects = Physics.OverlapCapsule(CamPos.position, CamPos.position + (CamPos.forward * capsuleLength), capsuleRadius, 1024);
+
+            foreach (Collider viewedObjectsCol in viewedObjects)
+            {
+                if (viewedObjectsCol.gameObject.GetComponent<ChangeTheWorld>() && !viewedObjectsCol.gameObject.GetComponent<ChangeTheWorld>().isChanged)
+                {
+                    ChangeTheWorld compatibleObject = viewedObjectsCol.gameObject.GetComponent<ChangeTheWorld>();
+                    compatibleObject.ChangeSomething.Invoke();
+                    compatibleObject.isChanged = true;
+                }
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        if (discoActive)
+        {
+            Gizmos.DrawSphere(CamPos.position, capsuleRadius);
+            Gizmos.DrawSphere(CamPos.position + (CamPos.forward * capsuleLength), capsuleRadius);
+        }
+    }
+
+    public void FPScount()
+    {
+        fps = 1 / Time.deltaTime;
+        fps = Mathf.Round(fps);
+        timetxt.text = "" + timeScale;
+        fpstxt.text = "" + fps;
+        if (fps >= 80)
+        {
+            fpstxt.color = Color.green;
+        }
+        else
+        {
+            fpstxt.color = Color.yellow;
+        }
+    }
+
+    public void Transition()
+    {
+
+    }
+
+    public void TransitionEnd()
+    {
+
+        discoActive = true;
+        foreach (AudioSource As in AudioSources)
+        {
+            As.Play();
+        }
+    }
+
+    public void Timer()
+    {
+        timers += Time.deltaTime;
+
+        bool premier = false;
+
+        if(timers > 10 && !premier)
+        {
+            UI_Micro.GetComponent<Animator>().SetBool("isActive", true);
+            premier = true;
+        }
+    }
+
+
+}
---
+++ Fabrisco_BIRP/Assets/_TEST/SC_Disc.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class SC_Disc : MonoBehaviour
+{
+    public AudioSource[] AudioSources;
+    public Animator[] ans, ansss;
+    public List<Animator> anss;
+    public AnimationState anims;
+    public CircularDrive circularDrive;
+    public Interactable interactComponent;
+    public LinearMapping linearMapping;
+    public float outAngle;
+    public float rotationValue;
+    public float linearMapValue;
+    public float globalMultiplier;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        AudioSources = FindObjectOfType<GameManager>().AudioSources;
+        ans = FindObjectsOfType<Animator>();
+        outAngle = circularDrive.outAngle;
+        circularDrive = GetComponent<CircularDrive>();
+        interactComponent = GetComponent<Interactable>();
+        linearMapping = GetComponent<LinearMapping>();
+    }
+    void Start()
+    {
+        foreach(Animator ani in ans)
+        {
+            if(ani.gameObject.tag == "reversable")
+            {
+                anss.Add(ani);
+            }
+        }
+
+        ansss = anss.ToArray();
+    }
+
+    public void Update()
+    {
+        transform.Rotate(0, rotationValue * Time.deltaTime, 0, Space.World);
+        if (linearMapping.value > linearMapValue)
+        {
+            Accelerate(linearMapping.value - linearMapValue);
+        }
+        else if (linearMapping.value < linearMapValue)
+        {
+            Decelerate(linearMapValue - linearMapping.value);
+        }
+        else
+        {
+            Reset();
+        }
+        linearMapValue = linearMapping.value;
+    }
+
+    // Update is called once per frame
+    public void Accelerate(float multiplier)
+    {
+        foreach (AudioSource As in AudioSources)
+        {
+            As.pitch += multiplier * globalMultiplier;
+        }
+
+        foreach (Animator an in ansss)
+        {
+            an.SetFloat("Multiplier", 1 + (multiplier * globalMultiplier));
+        }
+    }
+
+    public void Decelerate(float multiplier)
+    {
+        foreach (AudioSource As in AudioSources)
+        {
+            if(As.pitch <= -1)
+            {
+                As.pitch = -1;
+            }
+            else
+            {
+                As.pitch -= multiplier * globalMultiplier;
+            }
+        }
+        foreach (Animator an in ansss)
+        {
+            an.SetFloat("Multiplier", -1 - (multiplier * globalMultiplier));
+        }
+    }
+
+    public void Reset()
+    {
+        foreach (AudioSource As in AudioSources)
+        {
+            As.pitch = 1f;
+        }
+        foreach (Animator an in ansss)
+        {
+            an.SetFloat("Multiplier", 1f);
+        }
+    }
+}
