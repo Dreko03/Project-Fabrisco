acbd1246ede1622d056b0ea8fb14fc4cd392ce71
DrekoVR <Loic.pauleau@gmail.com>
2019-12-17 17:04:37 +0100

    Changes

---
+++ Fabriscoo/Assets/_Scripts/GPE/Ch_DJSet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ch_DJSet : MonoBehaviour
+{
+    Animator an;
+
+    private void Awake()
+    {
+        an = GetComponentInChildren<Animator>();
+    }
+
+    public void Change()
+    {
+        an.SetBool("Change", true);
+    }
+
+    public void Unchange()
+    {
+        an.SetBool("change", false);
+    }
+}
